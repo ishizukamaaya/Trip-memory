8dbf47dc62eb57ce1da6e79b50cd0aa855c5f515
EC2 Default User <maayajas@gmail.com>
2021-10-06 03:25:20 +0000

    controller-create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  # get 'homes/index'
+
   root "homes#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "home/about" => "homes#about"
+
+  resources :users,only:[:show,:edit,:update,:unsubscribe,:destroy] do
+    resources :relationships,only:[:create,:destroy]
+    get "followings" => "relationships#followings", as: "followings"
+    get "followers" => "relationships#followers", as: "followers"
+  end
+
+  resources :post_images do
+    resources :favorites,only:[:create,:destroy]
+    resources :comments,only:[:create,:destroy]
+  end
+
+  resources :tag,only:[:index]
+
 end
