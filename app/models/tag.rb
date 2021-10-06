0d80659652b5e7e89b689c3982cdd71ba5e5d21e
EC2 Default User <maayajas@gmail.com>
2021-10-06 07:27:13 +0000

    model-association

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,6 @@
 class Tag < ApplicationRecord
+
+  has_many :post_image_tags, dependent: :destroy
+  has_many :post_images, through: :post_image_tags
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :post_images,dependent: :destroy
+  has_many :favorites,dependent: :destroy
+  has_many :comments,dependent: :destroy
+  
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "folloewd_id", dependent: :destroy #フォローされてる人を取得
+  has_many :followers, through: :reverse_of_relationships, source: :follower #自分をフォローしてる人
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy　#フォローしてる人を取得
+  has_many :followings, through: :relationships, source: :folloewd #自分がフォローしてる人
+  
+  has_many :post_image_tags, dependent: :destroy
+  has_many :tags, through: :post_image_tags
+
+
 end
