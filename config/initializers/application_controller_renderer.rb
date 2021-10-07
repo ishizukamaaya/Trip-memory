1d928fd763b2381d6fbea1620f1a39204ae65a53
EC2 Default User <maayajas@gmail.com>
2021-10-07 08:37:12 +0000

    post_image-create

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '560b8b76c3f02dfabfdea64a0375160e338e56f326ed330f3676a83ac92c03bed49ae9193eeac9bc147f8c7350484e1b851f49f5ad53fc27370af7a5d8013d32'
+
